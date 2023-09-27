3d22a2cfba21d2e830b24558e37488804b881ae1
Mario Brito <79857158+britomario@users.noreply.github.com>
2023-09-27 10:45:21 -0300

    feature: calculadora de IMC

---
+++ Components/Calculadora/index.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import styles from './calculadora.module.css';
+import '/global.css';
+
+function Calculadora() {
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('')
+    const [resultado, setResultado] = useState(null);
+    
+    function calculaImc () {
+        if(peso && altura) {
+            const alturaEmMetros = altura / 100;
+            const imc = peso / (alturaEmMetros * alturaEmMetros)
+            setResultado(imc.toFixed(2));
+        } else {
+            setResultado(null)
+        }
+
+    };
+
+    return (
+        <div className='container'>
+            <div className={styles.containerImc}>
+                <h1>Calculadora de IMC</h1>
+                <div>
+                    <label htmlFor='peso'><strong>Peso (kg): </strong> </label>
+                    <input 
+                    onChange={({ target }) => setPeso(target.value)} 
+                    type="number" 
+                    value={peso}
+                    id='peso'
+                    placeholder='insira seu peso ex: (80.50)' />
+                </div>
+                <div>
+                    <label htmlFor='altura'><strong>Altura (cm): </strong></label>
+                    <input 
+                    onChange={({ target }) => setAltura(target.value)} 
+                    type="number" 
+                    value={altura}
+                    id='altura'
+                    placeholder='insira sua altura ex: (175)' />
+                </div>
+                <button onClick={calculaImc}>Calcular</button>
+                {resultado && (
+                    <div>
+                        <h2>Seu IMC é <span>{resultado}</span></h2>
+                        <img src="./img/tabela-imc.png" alt="tabela-imc" />
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Calculadora;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Calculadora from "../Components/Calculadora";
+
+function App () {
+  return (
+    <>
+    <Calculadora />
+    </>
+  )
+}
+
+export default App;
